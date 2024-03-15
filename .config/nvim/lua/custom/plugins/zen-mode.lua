c3a8312d2e8d6dc4c75b5edfd8e7c9c9e011773d
Konbor704 <konbor12@gmail.com>
2024-03-15 18:57:25 +0100

    Initial commit

---
+++ .config/nvim/lua/custom/plugins/zen-mode.lua
@@ -0,0 +1,20 @@
+return {
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			window = {
+				backdrop = 1,
+				width = 85,
+			},
+			plugins = {
+				options = {
+					enabled = true,
+				},
+				kitty = {
+					enabled = false,
+					font = "+4",
+				},
+			},
+		},
+	},
+}

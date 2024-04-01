d6a0c39c3308204938692046f28c048d1cd61afb
Konbor704 <konbor12@gmail.com>
2024-04-01 19:57:16 +0200

    test

--- install.c
+++ install.c
@@ -2,15 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-  FILE *ftpr;
-  system("cat list.txt");
+#define MAX_LINES 1000
 
+void parser() {
+  FILE *f;
+  f = fopen("list.conf", "r");
+  if (f == NULL) {
+    printf("Failed to open file\n");
+    return;
+  }
+  char *file_content = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  char *lines[MAX_LINES];
+  int line_count = 0;
+
+  while ((read = getline(&file_content, &len, f)) != -1) {
+    if (file_content[0] != '#') {
+      lines[line_count] = malloc(read + 1);
+      if (lines[line_count] == NULL) {
+        printf("Failed to allocate memory for line\n");
+        break;
+      }
+      strcpy(lines[line_count], file_content);
+      line_count++;
+    }
+  }
+
+  free(file_content);
+  fclose(f);
+
+  char all_lines[100];
+
+  for (int i = 0; i < line_count; i++) {
+    // printf("Line %d: %s", i + 1, lines[i]);
+    strcat(all_lines, lines[i]);
+    free(lines[i]);
+  }
+  printf("%s", all_lines);
+}
+
+int main() {
   printf("Are those programs that you want to install?\n Enter yes or no:\n");
 
   char confirm[10];
+  fgets(confirm, sizeof(confirm), stdin);
+  confirm[strcspn(confirm, "\n")] = 0;
 
-  scanf("%9s", confirm);
   char no[] = "no";
   int result = strcmp(confirm, no);
 
@@ -19,22 +58,7 @@ int main() {
     exit(0);
   }
 
-  ftpr = fopen("list.txt", "r");
-  if (ftpr == NULL) {
-    printf("Error opening file that contains list of softwear. Make sure that "
-           "it exists.\n");
-    return 1;
-  }
-
-  char file_content[100];
-
-  while (fgets(file_content, sizeof(file_content), ftpr)) {
-    char command[] = "sudo pacman -S ";
-    strcat(command, file_content);
-    system(command);
-  }
-
-  fclose(ftpr);
+  parser();
 
   system("stow .");
 
